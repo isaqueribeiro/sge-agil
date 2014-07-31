2fd634774f3e41342d3bab4b3be8228714c5048c
isaque_programmer@yahoo.com.br <isaque_programmer@yahoo.com.br@f1f1a73a-ff63-1b24-0c32-92696c35a027>
2014-07-31 20:13:39 +0000

    Impressao em ECF (Não Fiscal)

---
+++ trunk/Sys/lib/EcfFactory.pas
@@ -0,0 +1,7 @@
+unit EcfFactory;
+
+interface
+
+implementation
+
+end.
---
+++ trunk/Sys/lib/EcfWindowsPrinter.pas
@@ -0,0 +1,18 @@
+unit EcfWindowsPrinter;
+
+interface
+
+Uses
+  SysUtils, StrUtils, Classes, UEcfAgil, UFuncoes;
+
+  Type
+    TEcfWindowsPrinter = class(TEcfAgil)
+    private
+
+    public
+
+  end;
+
+implementation
+
+end.
