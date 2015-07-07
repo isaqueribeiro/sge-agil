27fa5a59cd50e86c9f4d475f15ea2bc6f1a33aa7
isaque_programmer@yahoo.com.br <isaque_programmer@yahoo.com.br@f1f1a73a-ff63-1b24-0c32-92696c35a027>
2015-07-07 21:16:22 +0000

    Ajuste Backup

--- _diversos/Backups_Automatico/Backup_Automatico_20150115.bat
+++ _diversos/Backups_Automatico/Backup_Automatico_20150115.bat
@@ -7,23 +7,30 @@ rem pastabackupfinalsys
 rem pastarelatoriossys
 rem googledrivesys
 
-set pastabackuptmpsys=D:\Projetos\ASS\SGE\_diversos\Backups_Automatico
+set pastabackuptmpsys=E:\Projetos\Agil Solucoes em Softwares\SGE\_diversos\Backups_Automatico
 set stringbancosys=localhost:DIMENSAO
 set nomearquivosys=TESTE
-set pastabackupfinalsys=D:\Projetos\ASS\SGE\_diversos\Backups_Automatico\Bkp
-set pastarelatoriossys=D:\Projetos\ASS\SGE\_diversos\Backups_Automatico\Log
-set pastafirebird=C:\Program Files (x86)\Firebird\Firebird_2_5\bin
+set pastabackupfinalsys=E:\Projetos\Agil Solucoes em Softwares\SGE\_diversos\Backups_Automatico\Bkp
+set pastarelatoriossys=E:\Projetos\Agil Solucoes em Softwares\SGE\_diversos\Backups_Automatico\Log
+set pastafirebird=C:\Program Files (x86)\Firebird\Firebird_2_1\bin
 set pastazip7=C:\Program Files\7-Zip
-set googledrivesys=C:\Users\Isaque\Google Drive\_Masterdados
+set googledrivesys=C:\Users\Isaque\Google Drive\_Backup%nomearquivosys%
+
+del "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.fbk"
+del "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.log"
+del "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.7z"
+
+del "%pastarelatoriossys%\%nomearquivosys%_*.*"
 
 "%pastafirebird%\gbak.exe" -b -z -v "%stringbancosys%" "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.fbk" -Y "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.log" -user SYSDBA -password masterkey
 
-"%pastazip7%\7z.exe" a -t7z -mx9 "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.7z" "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.*"
+"%pastazip7%\7z.exe" a -t7z -mx9 "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.7z" "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.*" "%pastarelatoriossys%"
 
 copy "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.7z" "%pastabackupfinalsys%"
 copy "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.log" "%pastarelatoriossys%"
 
 copy "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.7z" "%googledrivesys%"
 
+del "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.fbk"
 del "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.log"
 del "%pastabackuptmpsys%\%nomearquivosys%_%ymd%.7z"
